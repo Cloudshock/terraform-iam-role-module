c14cf2282b7c1a5acd549a72685a0ec1f9149285
Marc Boudreau <marc.a.boudreau@gmail.com>
2023-01-01 12:49:04 -0500

    added git commit hash to repository name value in tests

--- tests/module_test.go
+++ tests/module_test.go
@@ -2,16 +2,34 @@ package tests
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/iam"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
+var gitCommitHash string
+
+func getGitCommitHash(t *testing.T) string {
+	r, err := git.PlainOpen("../")
+	require.NoError(t, err)
+
+	h, err := r.ResolveRevision(plumbing.Revision("HEAD"))
+	require.NoError(t, err)
+
+	return h.String()[0:7]
+}
+
 func TestModule(t *testing.T) {
+	gitCommitHash = getGitCommitHash(t)
+
 	initOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: ".",
 	})
@@ -35,11 +53,11 @@ func ModuleTest1(t *testing.T) {
 				"ec2:CreateVpc",
 				"ec2:DeleteVpc"
 			],
-			"Resource": "arn:aws:ec2:*:*:vpc/test-1-*"
+			"Resource": "arn:aws:ec2:*:*:vpc/test-1"
 		}
 	]
 }`,
-			"test_name": "test-1",
+			"test_name": fmt.Sprintf("terraform-iam-role-module-test-%s-1", gitCommitHash),
 		},
 	})
 
@@ -51,18 +69,19 @@ func ModuleTest1(t *testing.T) {
 	}
 
 	cfg, err := config.LoadDefaultConfig(context.TODO())
-	assert.NoError(t, err)
+	require.NoError(t, err)
+	cfg.Region = "ca-central-1"
 
 	iamClient := iam.NewFromConfig(cfg)
 
 	roleOutput, err := iamClient.GetRole(context.TODO(), &iam.GetRoleInput{
-		RoleName: aws.String("test-1"),
+		RoleName: aws.String(fmt.Sprintf("terraform-iam-role-module-test-%s-1", gitCommitHash)),
 	})
 	assert.NoError(t, err)
 
 	assert.NotNil(t, roleOutput.Role.AssumeRolePolicyDocument)
 	assert.Contains(t, *roleOutput.Role.AssumeRolePolicyDocument, "sts%3AAssumeRole")
-	assert.Contains(t, *roleOutput.Role.AssumeRolePolicyDocument, "aws%3APrincipalTag%2Fassume-test-1")
+	assert.Contains(t, *roleOutput.Role.AssumeRolePolicyDocument, fmt.Sprintf("aws%%3APrincipalTag%%2Fassume-terraform-iam-role-module-test-%s-1", gitCommitHash))
 
 	rolePolicyOutput, err := iamClient.GetRolePolicy(context.TODO(), &iam.GetRolePolicyInput{
 		RoleName:   roleOutput.Role.RoleName,
